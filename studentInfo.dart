0694402e0e7993ee984212ae56932c682a5bc8f0
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-03 06:29:40 +0600

    Data Type

---
+++ studentInfo.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('Hello, Dart!');
+
+  var name = 'Alice';
+  int age = 30;
+  double height = 5.5;
+  bool isStudent = false;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+}
