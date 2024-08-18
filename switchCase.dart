a2286a323904c3846380e689828cda243816c615
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-19 00:27:10 +0600

    Switch Case

---
+++ switchCase.dart
@@ -0,0 +1,22 @@
+void main() {
+  int day = 3;
+  switch (day) {
+    case 1:
+      print('Monday');
+      break;
+    case 2:
+      print('Tuesday');
+      break;
+    case 3:
+      print('Wednesday');
+      break;
+    case 4:
+      print('Thursday');
+      break;
+    case 5:
+      print('Friday');
+      break;
+    default:
+      print('Weekend');
+  }
+}
