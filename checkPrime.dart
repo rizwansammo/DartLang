23ddf56b7e59765ca23cccabb94f30903141d1ec
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-19 00:35:50 +0600

    Check Prime Number

---
+++ checkPrime.dart
@@ -0,0 +1,17 @@
+void main() {
+  int number = 17;
+  bool isPrime = true;
+
+  for (int i = 2; i <= number ~/ 2; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    print('$number is a prime number');
+  } else {
+    print('$number is not a prime number');
+  }
+}
---
+++ dartFunction.dart
@@ -0,0 +1,8 @@
+void main() {
+  int result = multiply(5, 4);
+  print('Product: $result');
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
