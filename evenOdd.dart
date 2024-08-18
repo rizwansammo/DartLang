d04700a7ca55c7266662af26ce45aec080bf09ae
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-19 00:19:08 +0600

    Find the largest Number

---
+++ evenOdd.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number = 7;
+  if (number % 2 == 0) {
+    print('$number is even');
+  } else {
+    print('$number is odd');
+  }
+}
---
+++ findLarge.dart
@@ -0,0 +1,6 @@
+void main() {
+  int a = 15;
+  int b = 25;
+  int largest = (a > b) ? a : b;
+  print('Largest Number: $largest');
+}
