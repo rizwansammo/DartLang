9702e15fc70e227fdd080ee25fc1cdb336362857
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-12 17:39:20 +0600

    If-else if-else ladder

---
+++ ifElseLadder.dart
@@ -0,0 +1,13 @@
+void main() {
+  int score = 75;
+
+  if (score >= 90) {
+    print('Grade: A');
+  } else if (score >= 80) {
+    print('Grade: B');
+  } else if (score >= 70) {
+    print('Grade: C');
+  } else {
+    print('Grade: F');
+  }
+}
