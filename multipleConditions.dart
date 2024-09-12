b7855fb591ae3156581cfbc6e85340c1ec29a0e6
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-12 17:39:55 +0600

    Checking multiple conditions with logical operators

---
+++ multipleConditions.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  int b = 20;
+
+  if (a > 0 && b > 0) {
+    print('Both a and b are positive.');
+  } else {
+    print('One or both numbers are not positive.');
+  }
+}
